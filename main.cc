4e5611ecb639d7b42cda1d726ad2f460acf295b3
thulasiy1 <54640009+thulasiy1@users.noreply.github.com>
2021-05-21 16:16:35 -0700

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "cpputils/graphics/image.h"
+#include "game.h"
+#include "game_element.h"
+#include "opponent.h"
+#include "player.h"
+
+int main() {
+  Game game;
+  game.Init();
+  game.Start();
+}
